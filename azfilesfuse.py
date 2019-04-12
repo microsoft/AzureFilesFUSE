3f9adf223a9c2993a9d245e57c5dc78d710d5812
Geoffrey Troyer <getroyer@microsoft.com>
2019-04-12 15:13:29 -0700

    Changed folder permissions to 750, added a few comments

--- azfilesfuse.py
+++ azfilesfuse.py
@@ -205,9 +205,9 @@ class AzureFiles(LoggingMixIn, Operations):
             st['st_uid'] = uid
             st['st_gid'] = gid
 
-
+            # the permissions should probably be parameterized before a merge to master
             if path == '':
-                st['st_mode'] = stat.S_IFDIR | 0o770
+                st['st_mode'] = stat.S_IFDIR | 0o750 # TODO parameterize folder permissions
                 st['st_nlink'] = 2
                 return st
             
@@ -218,12 +218,12 @@ class AzureFiles(LoggingMixIn, Operations):
                 raise FuseOSError(ENOENT)
 
             if isinstance(item, models.Directory):
-                st['st_mode'] = stat.S_IFDIR | 0o770
+                st['st_mode'] = stat.S_IFDIR | 0o750 # TODO parameterize subfolder permissions
                 st['st_nlink'] = 2
                 properties = self._files_service.get_directory_properties(
                     self._azure_file_share_name, path).properties
             else:
-                st['st_mode'] = stat.S_IFREG | 0o770
+                st['st_mode'] = stat.S_IFREG | 0o770 # TODO parameterize file permissions
                 st['st_nlink'] = 1
                 st['st_size'] = item.properties.content_length
                 properties = self._files_service.get_file_properties(
